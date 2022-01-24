8e66c39792cfb33846e6d7a30e85c564a5d370e1
Peszko-Patryk <patrickpeszko@gmail.com>
2022-01-24 15:14:52 +0100

    final for data security class

--- finish/app.py
+++ finish/app.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
+from flask_sqlalchemy  import SQLAlche
+my
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
